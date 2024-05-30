ca2f127524c2b4ba5d61f666edd899de26719496
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-05-30 14:40:38 -0400

    Update WebSocketEventListener.java

--- src/main/java/com/joon/chat/WebSocketEventListener.java
+++ src/main/java/com/joon/chat/WebSocketEventListener.java
@@ -1,5 +1,21 @@
 
 package com.joon.chat;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ) {
+        // TODO: -- to be implemented
+    }
 }
